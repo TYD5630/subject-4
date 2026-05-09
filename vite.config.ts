4b130d258c1f4243b1c48811efe9576156ebe865
Mr.T <mrt@example.com>
2026-05-09 20:29:57 +0800

    fix: dynamic base path for Vercel vs GitHub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/subject-4/',
+  base: process.env.VERCEL ? '/' : '/subject-4/',
 })
